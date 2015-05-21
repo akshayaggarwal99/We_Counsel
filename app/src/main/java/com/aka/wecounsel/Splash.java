ca44e26bd452f5a5141757eb899b0c5de94e9902
akshayaggarwal99 <akshayaggarwal99@gmail.com>
2015-05-22 02:16:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/aka/wecounsel/Splash.java
@@ -0,0 +1,78 @@
+package com.aka.wecounsel;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+public class Splash extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        LayoutInflater inflater = getLayoutInflater();
+        View appearance = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(R.id.ll_first));
+        toast.setView(appearance);
+        toast.show();
+        Thread th = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i= new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(i);
+
+                }
+            }
+
+        };
+        th.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
